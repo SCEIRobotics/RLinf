c6321cad9afc61b7f600029dec86d8e047251757
xingchen <chen.xing@changhong.com>
2025-12-17 09:46:34 +0000

    add flower sample actions, TODO: update value head input, actions postprocess, forward

---
+++ rlinf/models/embodiment/flower_action_model.py
@@ -0,0 +1,448 @@
+import math
+import random
+from dataclasses import dataclass, field
+from typing import Any, Literal
+import numpy as np
+import torch
+from torch import Tensor
+import torch.nn.functional as F
+from torchvision import transforms
+from lerobot.policies.flower.modeling_flower import FlowerModel
+from lerobot.policies.flower.configuration_flower import FlowerConfig
+from lerobot.datasets.streaming_dataset import FlowerDataCollator
+
+from rlinf.models.embodiment.modules.explore_noise_net import ExploreNoiseNet
+from rlinf.models.embodiment.modules.value_head import ValueHead
+
+@dataclass
+class FlowerRLConfig(FlowerConfig):
+    # config for rl
+    config_name: str = (
+        "flower"  # pi0_libero, pi05_libero, pi0_metaworld, pi05_metaworld
+    )
+
+    robot_type: str = "genie1"
+
+    # noise configs
+    noise_method: str = "flow_sde"  # flow_sde, flow_noise, flow_cps
+    # noise config for flow-sde
+    noise_level: float = 0.5
+    noise_anneal: bool = False
+    noise_params: list = field(
+        default_factory=lambda: [0.7, 0.3, 400]
+    )  # noise_start, noise_end, noise_anneal_steps
+    # noise config for flow-noise
+    noise_logvar_range: list = field(
+        default_factory=lambda: [0.08, 0.16]
+    )  # [min_std, max_std]
+
+    # hyper-parameters
+    action_chunk: int = 16  # action chunk
+    action_env_dim: int = 7  # for environment action dim
+    max_action_dim: int = 16  # for max action dim
+    # num_steps: int = 10  # denoise steps
+
+    # training config
+    train_expert_only: bool = False
+    safe_get_logprob: bool = False
+    joint_logprob: bool = False  # designed for flow-noise
+    double_layer: bool = False  # designed for flow-sde without acceleration
+    ignore_last: bool = False  # ignore the last action for noise injection
+
+    # critic
+    detach_critic_input: bool = False  # detach critic input with the action expert
+    chunk_critic_input: bool = False  # use only the action chunk for critic estimation
+    add_value_head: bool = False  # add value head for ppo
+    value_vlm_mode: str = "mean_token"  # last_token, mean_token, first_token
+
+    def __post_init__(self):
+        object.__setattr__(self, "action_chunk", self.horizon)
+    
+class FlowerTokenizer(FlowerDataCollator):
+    def __init__(self, vlm_path: str):
+        super().__init__(vlm_path)
+        self.processor = None
+
+    def __call__(self, batch, robot_type: str):
+        task_batch = batch["task_descriptions"]
+        robot_batch = [robot_type] * len(task_batch)
+
+        constructed_prompts, batch_action_index = self.construct_prompts(task_batch, robot_batch)
+        text_inputs = self.tokenizer(
+            constructed_prompts,
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
+            max_length=77
+        )
+
+        result = {}
+        result['text_input_ids'] = text_inputs['input_ids']
+        result['text_attention_mask'] = text_inputs.data["attention_mask"]
+        result['action_index'] = batch_action_index
+        return result
+
+class FlowerForRLActionPrediction(FlowerModel):
+    def __init__(self, config: FlowerRLConfig):
+        super().__init__(config)
+        self.global_step = 0
+
+        # value head
+        proj_width = 1024
+        if self.config.add_value_head:
+            self.value_head = ValueHead(
+                input_dim=proj_width,
+                hidden_sizes=(512, 256, 128),
+                output_dim=1,
+                activation="relu",
+                bias_last=True,
+            )
+            
+            # noise head for flow-noise
+        if self.config.noise_method == "flow_noise":
+            self.noise_head = ExploreNoiseNet(
+                in_dim=1024,
+                out_dim=self.config.max_action_dim,
+                hidden_dims=[128, 64],
+                activation_type="tanh",
+                noise_logvar_range=self.config.noise_logvar_range,
+                noise_scheduler_type="learn",
+            )
+        
+        # tokenizer
+        self.tokenizer = FlowerTokenizer(config.vlm_path)
+
+        # other
+        self.img_transform = transforms.Normalize(
+                            mean=[0.485, 0.456, 0.406],  # RGB 均值
+                            std=[0.229, 0.224, 0.225]    # RGB 标准差
+                        )
+    
+    def predict_action_batch(
+        self, env_obs, mode: Literal["train", "eval"] = "train", compute_values=True
+    ) -> tuple[np.ndarray, dict[str, Any]]:
+        
+        processed_obs = self.preprocess_observations(env_obs)
+        # sample actions
+        outputs = self.sample_actions(
+            processed_obs, mode=mode, compute_values=compute_values
+        )
+
+        # post process actions
+        actions = outputs["actions"][:, :self.config.action_chunk, :self.config.action_env_dim].cpu().numpy()
+        actions = self.postprocess_actions(actions)
+
+        forward_inputs = {
+            "chains": outputs["chains"],
+            "denoise_inds": outputs["denoise_inds"],
+        }
+        forward_inputs.update(processed_obs)
+        
+        result = {
+            "prev_logprobs": outputs["prev_logprobs"],
+            "prev_values": outputs["prev_values"],
+            "forward_inputs": forward_inputs,
+        }
+        return actions, result
+
+    def set_global_step(self, global_step):
+        self.global_step = global_step
+    
+    def sample_noise(self, shape, device):
+        return torch.normal(
+            mean=0.0,
+            std=1.0,
+            size=shape,
+            dtype=torch.float32,
+            device=device,
+        )
+    
+    def preprocess_observations(self, observation):
+        # match lerobot inputs
+        obs_out = {}
+        images = torch.stack([observation['images'], observation['wrist_images']], dim = 1)
+        obs_out['observation.images'] = images.unsqueeze(1)  # (B, n_obs_steps, num_cameras, C, H, W)
+        obs_out['observation.state'] = observation['states'].unsqueeze(1)  # (B, n_obs_steps, state_dim)
+
+        # resize images, normalize
+        batch_size, n_obs_steps, cam, channels, height, width = obs_out['observation.images'].shape
+        x_reshaped = obs_out['observation.images'].view(
+                    batch_size*n_obs_steps*cam,
+                    channels,
+                    height,
+                    width,
+                )
+        x_reshaped = F.interpolate(x_reshaped, size=(224,224), mode='bilinear').type(torch.float32) / 255.
+        x_reshaped = self.img_transform(x_reshaped)
+        obs_out['observation.images'] = x_reshaped.view(
+                    batch_size,
+                    n_obs_steps,
+                    cam,
+                    channels,
+                    224,
+                    224,
+                )
+        
+        # process text
+        text_inputs = self.tokenizer(observation, robot_type=self.config.robot_type)
+        # obs_out['text_input_ids'] = torch.ones(batch_size, 16, dtype=torch.int32, device=observation['states'].device)
+        # obs_out['text_attention_mask'] = torch.ones(batch_size, 16, dtype=torch.int32, device=observation['states'].device)
+        # obs_out['action_index'] = torch.ones(batch_size, dtype=torch.int32, device=observation['states'].device) * 2
+        obs_out.update(text_inputs)
+
+        return obs_out
+    
+    def postprocess_actions(self, actions):
+        return actions
+    
+    def get_logprob_norm(self, sample, mu, sigma):
+        # logprob = log p(x|mu,sigma) = -log(sigma) - 0.5 * log(2 * pi) - 0.5 * ((x - mu) / sigma) ** 2
+        if self.config.safe_get_logprob:
+            log_prob = -torch.pow((sample - mu), 2)
+        else:
+            mask = sigma == 0
+            sigma_safe = torch.where(mask, torch.ones_like(sigma), sigma)
+            constant_term = -torch.log(sigma_safe) - 0.5 * torch.log(
+                2 * torch.pi * torch.ones_like(sample)
+            )
+            exponent_term = -0.5 * torch.pow((sample - mu) / sigma_safe, 2)
+            log_prob = constant_term + exponent_term
+            log_prob = torch.where(mask, torch.zeros_like(log_prob), log_prob)
+        return log_prob
+    
+    @torch.no_grad()
+    def sample_actions(
+        self,
+        observation,
+        noise=None,
+        mode="train",
+        compute_values=True,
+    ) -> torch.Tensor:
+        """Do a full inference forward and compute the action (batch_size x num_steps x num_motors)"""
+        bsize = observation["observation.images"].shape[0]
+        device = next(self.parameters()).device
+        self.device = device
+        if noise is None:
+            actions_shape = (bsize, self.config.action_chunk, self.config.max_action_dim)
+            noise = self.sample_noise(actions_shape, device)
+        
+        # Encode image features and concatenate them all together along with the state vector.
+        """
+        encode_observations(observation)
+        This function expects `batch` to have:
+        {
+            "observation.state": (B, n_obs_steps, state_dim)
+
+            "observation.images": (B, n_obs_steps, num_cameras, C, H, W)
+                AND/OR
+            "observation.environment_state": (B, n_obs_steps, environment_dim)
+        }
+        """
+        cond = self.encode_observations(observation)
+
+        # run sampling
+        samples = self.conditional_sample(bsize, device, mode=mode,cond=cond, noise=noise, compute_values=compute_values)
+
+        return samples
+    
+    def conditional_sample(
+        self,
+        batch_size: int,
+        device,
+        mode="train",
+        cond: Tensor | None = None,
+        noise: Tensor | None = None,
+        compute_values=True,
+    ) -> Tensor:
+        num_steps = self.num_inference_steps
+        # Sample prior.
+        x_t = noise
+        # add sde sample and traj collect
+        chains = []
+        log_probs = []
+        values = []
+        chains.append(x_t)
+
+        if self.config.joint_logprob:
+            initial_log_prob = self.get_logprob_norm(
+                x_t, torch.zeros_like(noise), torch.ones_like(noise)
+            )
+            log_probs.append(initial_log_prob)
+
+        # In the joint logprob mode, we need to sample the logprob for each denoise step
+        # In the non-joint logprob mode, only one denoise step is sampled and ode-sde mix sampling is used
+        # denoise index
+        if mode == "train":
+            if self.config.joint_logprob:
+                denoise_inds = torch.arange(num_steps, 0, -1)
+            else:
+                if self.config.ignore_last:
+                    denoise_inds = torch.tensor(
+                        [random.randint(1, num_steps - 1)] * num_steps
+                    )
+                else:
+                    denoise_inds = torch.tensor(
+                        [random.randint(1, num_steps)] * num_steps
+                    )
+        else:
+            denoise_inds = torch.tensor([-1] * num_steps)
+        denoise_inds = denoise_inds[None].repeat(batch_size, 1)
+
+         # Integration
+        dt = 1.0 / num_steps
+        dt_tensor = torch.tensor([dt] * batch_size, device=device) # (batch_size,)
+
+        for idx in range(num_steps, 0, -1):
+            # Predict velocity field
+            t_val = float(idx) / num_steps
+            t_tensor = torch.full((batch_size,), t_val, device=device)  # (batch_size,)
+            v_t = self.dit_forward(x_t, t_tensor, cond)
+
+            # sample mean var val
+            if idx == denoise_inds[0][idx-1]:
+                sample_mode = "train"
+            else:
+                sample_mode = "eval"
+            x_t_mean, x_t_std, value_t = self.sample_mean_var_val(
+                x_t,
+                v_t,
+                t_tensor,
+                dt_tensor,
+                sample_mode,
+                compute_values,
+            )
+            # Euler step - use new tensor assignment instead of in-place operation
+            x_t = x_t_mean + self.sample_noise(x_t.shape, device) * x_t_std
+            log_prob = self.get_logprob_norm(x_t, x_t_mean, x_t_std)
+            # store
+            values.append(value_t)
+            chains.append(x_t)
+            log_probs.append(log_prob)
+        
+        # process results
+        x_0 = x_t
+        chains = torch.stack(chains, dim=1)
+        # post process for logprob
+        log_probs = torch.stack(log_probs, dim=1)[
+            :, :, : self.config.action_chunk, : self.config.action_env_dim
+        ]
+        if self.config.joint_logprob:
+            log_probs = log_probs.mean(dim=1)
+        else:
+            log_probs = log_probs[
+                torch.arange(log_probs.shape[0]),
+                denoise_inds[:, 0],
+            ]
+        # post process for value
+        values = torch.stack(values, dim=1).mean(dim=-1, keepdim=True)
+        return {
+            "actions": x_0,
+            "chains": chains,
+            "prev_logprobs": log_probs,
+            "prev_values": values,
+            "denoise_inds": denoise_inds,
+        }
+    
+    def sample_mean_var_val(self, 
+                            x_t,
+                            v_t, 
+                            t_tensor,
+                            dt_tensor,
+                            mode,
+                            compute_values):
+        device = x_t.device
+
+        # build parameters
+        if self.config.noise_anneal:
+            # noise annealing
+            noise_start, noise_end, anneal_steps = self.config.noise_params
+            noise_level = (
+                noise_start
+                + (noise_end - noise_start)
+                * min(self.global_step, anneal_steps)
+                / anneal_steps
+            )
+            noise_level = torch.tensor(noise_level).to(device)
+        else:
+            # fixed noise level
+            noise_level = torch.tensor(self.config.noise_level).to(device)
+
+        # value prediction
+        if (
+            self.config.add_value_head
+            and compute_values
+        ):
+            suffix_out = v_t
+            # use chunk critic input
+            if self.config.chunk_critic_input:
+                suffix_out_value = torch.mean(
+                    suffix_out[:, : self.config.action_chunk], dim=1, keepdim=False
+                )
+            else:
+                suffix_out_value = torch.mean(suffix_out, dim=1, keepdim=False)
+            # detach critic input
+            if self.config.detach_critic_input:
+                suffix_out_value = suffix_out_value.detach()
+            value_t = self.value_head(suffix_out_value)[:, 0]
+        else:
+            value_t = torch.zeros((v_t.shape[0]), device=device)
+
+        # ode sde mix sampling
+        delta = dt_tensor[:, None, None].expand_as(x_t)
+        t_input = t_tensor[:, None, None].expand_as(x_t)
+        x0_pred = x_t - v_t * t_input
+        x1_pred = x_t + v_t * (1 - t_input)
+        if mode == "eval":
+            x0_weight = 1 - (t_input - delta)
+            x1_weight = t_input - delta
+            x_t_std = torch.zeros_like(t_input)
+        elif mode == "train":
+            if self.config.noise_method == "flow_sde":
+                sigma_i = (
+                    noise_level
+                    * torch.sqrt(
+                        t_input
+                        / (1 - torch.where(t_input == 1, t_input-delta, t_input))
+                    )
+                )
+                x0_weight = torch.ones_like(t_input) - (t_input - delta)
+                x1_weight = t_input - delta - sigma_i**2 * delta / (2 * t_input)
+                x_t_std = torch.sqrt(delta) * sigma_i
+            elif self.config.noise_method == "flow_cps":
+                pi = torch.pi
+                cos_term = torch.cos(pi * noise_level / 2).to(device)
+                sin_term = torch.sin(pi * noise_level / 2).to(device)
+                x0_weight = torch.ones_like(t_input) - (t_input - delta)
+                x1_weight = (t_input - delta) * cos_term
+                x_t_std = (t_input - delta) * sin_term
+            elif self.config.noise_method == "flow_noise":
+                x0_weight = 1 - (t_input - delta)
+                x1_weight = t_input - delta
+                x_t_std = self.noise_head(v_t)  # TODO: need to change
+            else:
+                raise ValueError(f"Invalid noise method: {self.config.noise_method}")
+            
+        x_t_mean = x0_pred * x0_weight + x1_pred * x1_weight
+        return x_t_mean, x_t_std, value_t
+
+if __name__ == "__main__":
+    import ipdb; ipdb.set_trace()
+    # model
+    config = FlowerRLConfig()
+    config.n_obs_steps = 1
+    config.vlm_path = "/mnt/data/xingchen/models/pretrained/Florence-2-large"
+    model = FlowerForRLActionPrediction(config)
+    print(model)
+    device = torch.device('cuda:7' if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.eval()
+
+    # data
+    imgs = torch.randn(2, 3, 224, 224).to(device)
+    states = torch.randn(2, 16).to(device)
+    prompts = ['put this cup on the table', 'move the block to the left']
+    obs = {'images': imgs, 'wrist_images': imgs.clone(), 'states': states, 'task_descriptions': prompts}
+
+    # inference
+    actions, results = model.predict_action_batch(obs)
+    print(actions.shape)
--- toolkits/env_test.py
+++ toolkits/env_test.py
@@ -1,9 +1,11 @@
 import os
 import hydra
 # from rlinf.config import validate_cfg
-from rlinf.envs.maniskill.maniskill_env import ManiskillEnv
+from rlinf.config import SupportedModel
+from rlinf.envs import get_env_cls
 from rlinf.models import get_model, get_vla_model_config_and_processor
-from rlinf.envs import action_utils
+from rlinf.envs.action_utils import prepare_actions
+# from rlinf.envs import action_utils
 import cv2
 import numpy as np
 import torch
@@ -11,54 +13,67 @@ from tqdm import tqdm
 
 os.environ['EMBODIED_PATH'] = '/mnt/data/xingchen/github/RLinf/examples/embodiment/config'
 
-def env_test(cfg):
-    env = ManiskillEnv(cfg.env.eval, seed_offset=0, total_num_processes=1)
-    extracted_obs, _, _, _, infos = env.step()
+def env_test(cfg, num_envs):
+    eval_env_cls = get_env_cls(cfg.env.eval.simulator_type, cfg.env.eval)
+    env = eval_env_cls(cfg.env.eval, num_envs = num_envs, seed_offset=0, total_num_processes=1)
+    extracted_obs, infos = env.reset()
     device = extracted_obs['images'].device
     # print(extracted_obs)
     # obs = extracted_obs['images'][0].cpu().numpy()
     # obs = cv2.cvtColor(obs.transpose(1, 2, 0).astype(np.uint8), cv2.COLOR_RGB2BGR)
     # cv2.imwrite('logs/temp/obs.jpg', obs)
 
-    import ipdb; ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
 
     max_step = 50
     for eval_step in tqdm(range(max_step)):
-        action = torch.randn(cfg.env.eval.num_envs, cfg.actor.model.action_dim).to(device)
+        action = torch.randn(num_envs, cfg.actor.model.action_dim).to(device)
         extracted_obs, step_reward, terminations, truncations, infos = env.step(action)
     env.flush_video()
 
-def model_test(cfg):
+def model_test(cfg, num_envs, model_dir):
+    # model path update
+    # import ipdb; ipdb.set_trace()
+    cfg.rollout.model.model_path = model_dir
+    cfg.actor.model.model_path = model_dir
+    cfg.actor.tokenizer.tokenizer_model = model_dir
     # env
-    env = ManiskillEnv(cfg.env.eval, seed_offset=0, total_num_processes=1)
-    extracted_obs, _, _, _, infos = env.step()
+    eval_env_cls = get_env_cls(cfg.env.eval.simulator_type, cfg.env.eval)
+    env = eval_env_cls(cfg.env.eval, num_envs = num_envs, seed_offset=0, total_num_processes=1)
+    extracted_obs, infos = env.reset()
     device = extracted_obs['images'].device
 
     # model
-    model = get_model(cfg.rollout.model_dir, cfg.actor.model)
-    model_config, input_processor = get_vla_model_config_and_processor(cfg.actor)
-    model.setup_config_and_processor(model_config, cfg, input_processor)
+    model = get_model(cfg.actor.model)
+    if SupportedModel(cfg.actor.model.model_type) in [
+            SupportedModel.OPENVLA,
+            SupportedModel.OPENVLA_OFT,
+        ]:
+        model_config, input_processor = get_vla_model_config_and_processor(cfg.actor)
+        model.setup_config_and_processor(model_config, cfg, input_processor)
+    # model_dict = torch.load("/mnt/data/xingchen/github/RLinf/logs/20251212-08:44:22/test_openvla/checkpoints/global_step_75/actor/ckpt.pt")
+    # model.load_state_dict(model_dict)
     model.to(device)
     model.eval()
 
-    import ipdb; ipdb.set_trace()
-
     # inference
-    max_step = cfg.algorithm.n_eval_chunk_steps
+    max_step = cfg.env.eval.max_episode_steps // cfg.actor.model.num_action_chunks
+    max_step = 10
     kwargs = dict(cfg.algorithm.sampling_params)
     kwargs["do_sample"] = True
     kwargs["use_cache"] = True
     kwargs["temperature"] = kwargs["temperature_eval"]
     for _ in tqdm(range(max_step)):
-        extracted_obs["states"] = None
-        extracted_obs["wrist_images"] = None
+        # extracted_obs["states"] = None
+        # extracted_obs["wrist_images"] = None
         actions, result = model.predict_action_batch(env_obs=extracted_obs, **kwargs)
-        chunk_actions = action_utils.prepare_actions_for_maniskill(
-            actions,
+        chunk_actions = prepare_actions(
+            raw_chunk_actions=actions,
+            simulator_type=cfg.env.train.simulator_type,
+            model_type=cfg.actor.model.model_type,
             num_action_chunks=cfg.actor.model.num_action_chunks,
             action_dim=cfg.actor.model.action_dim,
-            policy=cfg.actor.model.get("policy_setup", None),
-            action_scale=1.0)
+            policy=cfg.actor.model.get("policy_setup", None),)
         extracted_obs, step_reward, terminations, truncations, infos = env.chunk_step(chunk_actions)
     env.flush_video()
 
@@ -67,12 +82,12 @@ def model_test(cfg):
 )
 def main(cfg) -> None:
     # cfg = validate_cfg(cfg)
-    cfg.env.eval.init_params.control_mode = "arm_pd_ee_target_delta_pose_align2_gripper_pd_joint_pos"
-    cfg.env.eval.num_envs = 2
-    cfg.env.eval.video_cfg.video_base_dir = 'logs/temp/'
+    if cfg.env.eval.simulator_type == 'maniskill':
+        cfg.env.eval.init_params.control_mode = "arm_pd_ee_target_delta_pose_align2_gripper_pd_joint_pos"
+    cfg.env.eval.video_cfg.video_base_dir = 'logs/temp/libero_openvlaoft'
 
-    # env_test(cfg)
-    model_test(cfg)
+    # env_test(cfg, num_envs = 2)
+    model_test(cfg, num_envs = 2, model_dir = '/mnt/data/xingchen/github/RLinf/logs/20251212-08:44:22/test_openvla/checkpoints/global_step_75/actor/model')
 
 if __name__ == "__main__":
     main()
